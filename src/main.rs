d532ee53508e8bd916559b790aacf93582834206
Chris West (Faux) <git@goeswhere.com>
2023-09-01 16:21:41 +0100

    feat: seems to work

--- src/main.rs
+++ src/main.rs
@@ -2,123 +2,128 @@ use std::env;
 use std::time::Duration;
 
 use anyhow::{anyhow, Context, Result};
-use log::{debug, error, info, trace, warn};
+use log::{debug, info, trace, warn};
+use reqwest::Client;
 use rumqttc::ConnectionError::MqttState;
 use rumqttc::{Event, EventLoop, Incoming, MqttOptions, Outgoing, QoS, StateError};
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
 use time::OffsetDateTime;
 use tokio::time::{sleep, timeout};
 
-#[derive(Deserialize)]
-#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
-struct Device {
-    actual_temp: String,
-    device_id: i64,
-    heat_on: bool,
-    set_temp: String,
-    zone_name: String,
-}
-
-#[derive(Deserialize)]
-struct LiveData {
-    #[serde(rename = "HUB_TIME", with = "time::serde::timestamp")]
-    hub_time: OffsetDateTime,
-    devices: Vec<Device>,
-}
-
-#[derive(Serialize)]
+#[derive(Debug, Serialize)]
 #[serde(rename_all = "camelCase")]
-struct MqttMessage<'s> {
+struct MqttMessage {
     /// the date/time I believe to be true
     #[serde(with = "time::serde::rfc3339")]
     publish_time: OffsetDateTime,
 
-    /// the date/time the hub believes to be true
-    #[serde(with = "time::serde::rfc3339")]
-    hub_time: OffsetDateTime,
-
-    /// identifier for the hub (it's mac address)
-    hub_id: &'s str,
-
-    /// index of the device we're reporting data for
-    device_idx: i64,
-
-    /// where the device thinks it is
-    zone_name: &'s str,
+    /// identifier for the inverter (it's serial number)
+    inverter_id: String,
 
     /// what the device thinks the temperature is
-    temp_actual: f64,
+    temp: f64,
 
-    /// what temperature the device is targeting
-    temp_set: f64,
+    /// current power from the solar panels (watts)
+    current_power_watts: f64,
 
-    /// whether the device is calling for heat
-    heat_on: bool,
+    /// yield today (kWh)
+    yield_today_kwh: f64,
+
+    /// total yield (kWh) (broken)
+    yield_total_kwh: Option<f64>,
+}
+
+struct Req {
+    endpoint: String,
+    username: String,
+    password: String,
+}
+
+async fn fetch(client: &Client, req: &Req) -> Result<MqttMessage> {
+    let data = client
+        .get(&req.endpoint)
+        .basic_auth(&req.username, Some(&req.password))
+        .send()
+        .await?
+        .error_for_status()?
+        .text()
+        .await?;
+
+    let data = data
+        .trim_matches('\0')
+        .trim()
+        .split(';')
+        .collect::<Vec<&str>>();
+    if data.len() < 7 {
+        return Err(anyhow!("unexpected data: {:?}", data));
+    }
+
+    let serial = data[0];
+    let _firmware = data[1];
+    let _model = data[2];
+    let temp = parse_float(data[3])?;
+    let current_power_watts = parse_float(data[4])?;
+    let yield_today_kwh = parse_float(data[5])?;
+    let yield_total_kwh = data[6];
+
+    let yield_total_kwh = match yield_total_kwh {
+        "d" => None,
+        _ => Some(parse_float(yield_total_kwh)?),
+    };
+
+    if serial.len() != 16 || serial.starts_with('0') {
+        return Err(anyhow!("unexpected serial: {:?}", serial));
+    }
+
+    Ok(MqttMessage {
+        publish_time: OffsetDateTime::now_utc(),
+        inverter_id: serial.to_string(),
+        temp,
+        current_power_watts,
+        yield_today_kwh,
+        yield_total_kwh,
+    })
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     pretty_env_logger::init_timed();
 
-    let mut neohub = neohub::Client::from_env().context("loading neohub client")?;
-    debug!("connecting to hub");
-    let hub_id = neohub
-        .identify()
-        .await
-        .context("identifying neohub")?
-        .device_id;
-    info!("found hub with id {:?}", hub_id);
-    let hub_id_topic = hub_id
-        .chars()
-        .filter(|c: &char| c.is_ascii_alphanumeric())
-        .collect::<String>();
+    let client = reqwest::Client::builder()
+        .http1_title_case_headers()
+        .build()?;
+    let solis_addr = env_var("SOLIS_ADDR")?;
+    let req = Req {
+        endpoint: format!("http://{}/inverter.cgi", solis_addr),
+        username: "admin".to_string(),
+        password: "123456789".to_string(),
+    };
 
     let mut mqtt_opts = MqttOptions::new("neohub-mqtt", env_var("MQTT_HOST")?, 1883);
     mqtt_opts.set_keep_alive(Duration::from_secs(15));
     let (mqtt, mqtt_loop) = rumqttc::AsyncClient::new(
         mqtt_opts,
         // how much data to buffer in-memory, when the broker is down, before we crash out
-        (10 /* minutes */ * 60) / 5 /* seconds per publish */ * 10, /* devices */
+        (10 /* minutes */ * 60) / 5, /* seconds per publish */
     );
 
     let mqtt_loop = tokio::spawn(log_for_loop(mqtt_loop));
 
     let err = loop {
-        let data: LiveData = match neohub
-            .command_void(neohub::commands::GET_LIVE_DATA)
-            .await
-            .context("fetching live data from neohub")
-        {
-            Ok(data) => data,
+        let payload = match fetch(&client, &req).await {
+            Ok(payload) => payload,
             Err(e) => break e,
         };
-        let now = OffsetDateTime::now_utc();
 
-        for device in data.devices {
-            let topic = format!("neohub/{hub_id_topic}/SENSOR/{}", device.device_id);
-            let payload = MqttMessage {
-                publish_time: now,
-                hub_time: data.hub_time,
-                hub_id: &hub_id,
-                device_idx: device.device_id,
-                zone_name: &device.zone_name,
-                temp_actual: parse_temp(&device.actual_temp)
-                    .with_context(|| anyhow!("actual temp on device {}", device.device_id))?,
-                temp_set: parse_temp(&device.set_temp)
-                    .with_context(|| anyhow!("set temp on device {}", device.device_id))?,
-                heat_on: device.heat_on,
-            };
-            let payload = serde_json::to_string(&payload)?;
-            debug!("publishing: {topic} {payload}");
+        let topic = format!("solis/{}/SENSOR/scrape", payload.inverter_id);
+        let payload = serde_json::to_string(&payload)?;
+        debug!("publishing: {topic} {payload}");
 
-            mqtt.try_publish(&topic, QoS::AtLeastOnce, true, payload)
-                .with_context(|| anyhow!("sending mqtt message to {}", topic))?;
-        }
+        mqtt.try_publish(&topic, QoS::AtLeastOnce, true, payload)
+            .with_context(|| anyhow!("sending mqtt message to {}", topic))?;
 
         sleep(Duration::from_secs(5)).await;
     };
-    error!("neohub failed {err:?}, attempting to cleanup...");
-    info!("neohub disconnection: {:?}", neohub.disconnect().await);
 
     info!("pausing to allow mqtt to flush...");
     sleep(Duration::from_secs(5)).await;
@@ -141,9 +146,9 @@ fn env_var(name: &'static str) -> Result<String> {
     env::var(name).with_context(|| anyhow!("reading env var {name:?}"))
 }
 
-fn parse_temp(temp: &str) -> Result<f64> {
-    temp.parse()
-        .with_context(|| anyhow!("parsing a temperature: {:?}", temp))
+fn parse_float(s: &str) -> Result<f64> {
+    s.parse()
+        .with_context(|| anyhow!("parsing a float: {:?}", s))
 }
 
 async fn log_for_loop(mut ev: EventLoop) {
